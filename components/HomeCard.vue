eb98770bd658a5885f487a4fa34646946d982ad4
OliveLeb <lebel.olivier@wanadoo.fr>
2021-07-22 01:45:11 +0200

    HomeCard Components

---
+++ components/HomeCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+            <img :src="home.images[0]" style="width:200px"><br/>
+            <p>{{ home.title }}</p>
+            <p> {{home.location.address}} {{home.location.city}} {{home.location.state}} </p>
+            <p> {{ home.guests}} guests </p>
+            <p> {{ home.pricePerNight}} / nights </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        home: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
     <div style="display:flex">
         <div v-for="home in homes" :key="home.objectID" style="margin:10px;">
-            <img :src="home.images[0]" style="width:200px"><br/>
-            <p>{{ home.title }}</p>
-            <p> {{home.location.address}} {{home.location.city}} {{home.location.state}} </p>
-            <p> {{ home.guests}} guests </p>
-            <p> {{ home.pricePerNight}} / nights </p>
+            <HomeCard :home="home"/>
         </div>
     </div>
 </template>
 
 <script>
 import homes from '~/data/homes'
+import HomeCard from '~/components/HomeCard'
 
 export default {
+    components: {
+            HomeCard,
+        },
     data() {
         return {
             homes :homes.slice(0,3)
